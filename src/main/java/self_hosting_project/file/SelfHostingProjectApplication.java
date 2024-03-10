6e64044ff256a42006a6b6b3ce1637009d444331
inbaanbu <inbarasan19981230@gmail.com>
2024-03-10 12:09:11 +0530

    first commit

---
+++ src/main/java/self_hosting_project/file/SelfHostingProjectApplication.java
@@ -0,0 +1,13 @@
+package self_hosting_project.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfHostingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfHostingProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/self_hosting_project/file/SelfHostingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package self_hosting_project.file;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfHostingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
