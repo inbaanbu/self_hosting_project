e0025d30ecc668c23cedaaefeb04bcb55cfca011
inbaanbu <inbarasan19981230@gmail.com>
2024-03-11 08:29:46 +0530

    bug fix

--- src/main/java/self_hosting_project/file/SelfHostingProjectApplication.java
+++ src/main/java/self_hosting_project/file/SelfHostingProjectApplication.java
@@ -2,6 +2,7 @@ package self_hosting_project.file;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
@@ -10,7 +11,7 @@ public class SelfHostingProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SelfHostingProjectApplication.class, args);
 	}
-
+   @GetMapping("/api")
 	public String inba() {
 		return "hii inba self hosting is running";
 		
